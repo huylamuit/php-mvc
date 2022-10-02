ffb86e163478338fde1f273dc4205b7218c682e7
huylamuit <20521389@gm.uit.edu.vn>
2022-10-02 20:02:18 +0700

    test

---
+++ mvc/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+// http://localhost/live/Home/Show/1/2
+
+class Home extends Controller{
+
+    // Must have SayHi()
+    function SayHi(){
+        $this->view('pages/product-list');
+
+    }
+
+
+}
+?>
